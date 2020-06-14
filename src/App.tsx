06211f4ec7f9f8baacd8c52602b9170b59fdb9d7
MasanoriIwakura <hagobutler@yahoo.co.jp>
2020-06-14 18:23:59 +0900

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from "react";
-import { Admin, Resource } from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
-import { UserList } from "./components/User";
-import { PostList, PostCreate, PostEdit } from "./components/Post";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-// ここでAPI側のエンドポイントのルートを指定すれば良いみたい
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+import ReactAdmin from "./pages/ReactAdmin";
 
 const App = () => {
   return (
-    <Admin dataProvider={dataProvider}>
-      <Resource
-        name="posts"
-        list={PostList}
-        edit={PostEdit}
-        create={PostCreate}
-      />
-      <Resource name="users" list={UserList} />
-    </Admin>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/admin">
+          <ReactAdmin />
+        </Route>
+      </Switch>
+    </Router>
   );
+};
+
+const Home = () => {
+  return (
+    <>
+    <h2>Home</h2>
+    <ul>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/admin">ReactAdmin</Link>
+      </li>
+    </ul>
+    </>
+  );
+};
+
+const About = () => {
+  return <h2>About</h2>;
 };
 
 export default App;
